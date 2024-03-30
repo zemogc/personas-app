7412d453840a3cc15d8d61fe9fdad1675f29c204
Zemog Gomez <zemogmjg@gmail.com>
2024-03-29 21:18:40 -0500

    Ajuste de la vista del listado de comunas

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -16,7 +16,12 @@ class ComunaController extends Controller
      */
     public function index()
     {
-        $comunas = Comuna::all();
+        //
+       // $comunas = Comuna::all();
+       $comunas = DB::table('tb_comuna')
+       ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+       ->select('tb_comuna.*', "tb_municipio.muni_nomb")
+       ->get();
         return view('comuna.index',['comunas' => $comunas]);
     }
 
