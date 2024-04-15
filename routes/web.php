ae703b6509cd154ea0a4bbda61509073247db426
Zemog gomez <zemogmjg@gmail.com>
2024-04-14 20:02:24 -0500

    Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,46 +1,31 @@
 <?php
 
-use App\Http\Controllers\ComunaController;
-use App\Http\Controllers\PaisController;
-use App\Http\Controllers\MunicipioController; // Importar correctamente el controlador MunicipioController
-use App\Http\Controllers\DepartamentoController; //por el comentario de arriba ya tengo claro que aqui defino esta parte para cada entidad
-
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Rutas de comunas
-Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
-Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
-Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
-Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
-Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
-Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Rutas de municipios
-Route::get('/municipios', [MunicipioController::class, 'index'])->name('municipios.index');
-Route::post('/municipios', [MunicipioController::class, 'store'])->name('municipios.store');
-Route::get('/municipios/create', [MunicipioController::class, 'create'])->name('municipios.create'); // Ruta para mostrar el formulario de creación de municipio (ya aprendi y entedni el funcionamiento de todas estas rts y su importancia)
-Route::delete('/municipios/{municipio}', [MunicipioController::class, 'destroy'])->name('municipios.destroy');
-Route::put('/municipios/{municipio}', [MunicipioController::class, 'update'])->name('municipios.update');
-Route::get('/municipios/{municipio}/edit', [MunicipioController::class, 'edit'])->name('municipios.edit');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Rutas de departamentos
-Route::get('/departamentos', [DepartamentoController::class, 'index'])->name('departamentos.index');
-Route::post('/departamentos', [DepartamentoController::class, 'store'])->name('departamentos.store');
-Route::get('/departamentos/create', [DepartamentoController::class, 'create'])->name('departamentos.create');
-Route::delete('/departamentos/{departamento}', [DepartamentoController::class, 'destroy'])->name('departamentos.destroy'); //ruta ya configurada pasa a prueba
-Route::put('/departamentos/{departamento}', [DepartamentoController::class, 'update'])->name('departamentos.update'); //Ruta despues del edit Ok
-Route::get('/departamentos/{departamento}/edit', [DepartamentoController::class, 'edit'])->name('departamentos.edit'); //ruta configurada previamente esta Okk
-
-// Rutas de paises
-Route::get('/paises', [PaisController::class, 'index'])->name('paises.index');
-Route::get('/paises/create', [PaisController::class, 'create'])->name('paises.create');
-Route::post('/paises', [PaisController::class, 'store'])->name('paises.store');
-Route::get('/paises/{pais}/edit', [PaisController::class, 'edit'])->name('paises.edit');
-Route::put('/paises/{pais}', [PaisController::class, 'update'])->name('paises.update');
-Route::delete('/paises/{pais}', [PaisController::class, 'destroy'])->name('paises.destroy');
-
-
+require __DIR__.'/auth.php';
