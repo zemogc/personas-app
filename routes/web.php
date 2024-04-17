95aa2d9431a7c89febb0f2205865014517e3ed5c
Zemog gomez <zemogmjg@gmail.com>
2024-04-16 22:06:15 -0500

    actualizaciones

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComunaController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/comunas', [ComunaController::class,
+'index'])->name('comunas.index');
+Route::post('/comunas', [ComunaController::class,
+'store'])->name('comunas.store');
+Route::get('/comunas/create', [ComunaController::class,
+'create'])->name('comunas.create');
+Route::delete('/comunas/{comuna}', [ComunaController::class,
+'destroy'])->name('comunas.destroy');
+Route::put('/comunas/{comuna}', [ComunaController::class,
+'update'])->name('comunas.update');
+Route::get('/comunas/{comuna}/edit', [ComunaController::class,
+'edit'])->name('comunas.edit');
+
+
 
 Route::get('/', function () {
     return view('welcome');
